86897a5f621bae826746134bad0ea75c61996ed5
RAGHAB NEUPANE <neupaneraghab@gmail.com>
2026-05-15 12:14:33 +0545

    CORS implementation

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -3,12 +3,16 @@ package main
 import (
 	"Prive/internal/api"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	r := gin.Default()
 
+	r.Use(cors.Default())
+
 	r.POST("/upload", api.UploadHandler)
+
 	r.Run(":8080")
 }
